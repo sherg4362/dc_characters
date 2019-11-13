d4e7863fa9156062469212ec24edc650c8583285
sherg <github email address>
2019-11-13 17:11:17 -0500

    updated

---
+++ lib/dc_characters/CharacterFacts.rb
@@ -0,0 +1,10 @@
+module DCCharacters
+  class CharacterFacts
+    attr_accessor :powers, :occupation, :alias, :real_name, :back_story
+
+    def self.facts
+      puts "Details Working"
+    end
+
+  end
+end
